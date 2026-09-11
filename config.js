8e861a10099fdcbc35a563dac857d04cac0704ff
A1rzz <sebastiangutwald@gmail.com>
2026-09-11 03:06:09 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  import.meta.env.https://yyfvehzoacydmerritto.supabase.co,
-  import.meta.env.sb_publishable_33Paks2cQAh7aF7t_PwVPA_dhsvv3X-
-)
