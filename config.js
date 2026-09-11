8b45694761b162c1f203ae7ef49d5139fb444c20
A1rzz <sebastiangutwald@gmail.com>
2026-09-11 02:59:06 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  import.meta.env.https://yyfvehzoacydmerritto.supabase.co,
+  import.meta.env.sb_publishable_33Paks2cQAh7aF7t_PwVPA_dhsvv3X-
+)
